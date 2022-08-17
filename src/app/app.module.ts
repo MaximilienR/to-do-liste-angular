8fc51c5e65a39ec2c3a41a0a2b2e175c3a365fd3
[maximilien.roussel] <[ro.maximilien@gmail.com]>
2022-08-17 19:01:36 +0200

    firstcommit

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TechnoAddComponent } from './techno-add/techno-add.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    TechnoAddComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
